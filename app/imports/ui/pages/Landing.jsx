678aec463ede3f42afb04ecb43726eb85a74c9d3
jalenlum <808jalen@gmail.com>
2024-04-06 17:09:54 -1000

    fixed font color hover and active

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,7 +4,7 @@ import { Col, Container, Image, Row } from 'react-bootstrap';
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <>
-    <Image src="https://manoa.hawaii.edu/library/wp-content/uploads/2017/10/Sunny-Alcove.jpg" style={{ width:"100vw", filter:"brightness(0.6)", position:"absolute", zIndex:-1 }} />
+    <Image src="https://manoa.hawaii.edu/library/wp-content/uploads/2017/10/Sunny-Alcove.jpg" style={{ width:"99vw", filter:"brightness(0.6)", position:"absolute", zIndex:-1 }} />
     <Container id="landing-page" fluid className="py-3">
       <Row className="align-middle text-center">
        
