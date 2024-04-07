90046f3c5efb21f3655532d92713c8d758875627
Michelle Rasavong <94037287+MRasavong@users.noreply.github.com>
2024-04-07 13:34:53 -1000

    Reduce footer size and add icons

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -7,34 +7,36 @@ const Footer = () => {
   };
 
   return (
-    <footer style={{ paddingTop: '40px', paddingBottom: '40px', backgroundColor: 'black', color: 'white', position: 'fixed', bottom: '0', width: '100%', zIndex: '1000' }}>
+    <footer style={{ padding: '5px', backgroundColor: 'black', color: 'white', position: 'fixed', bottom: '0', width: '100%', zIndex: '1000' }}>
       <Container>
         <Row>
           <Col>
-            <h4 className="mb-2">Lost and Found at the University of Hawaii at Manoa</h4>
+            <h6 className="mb-1">Lost and Found at the University of Hawaii at Manoa</h6>
             <p className="mb-1">All rights reserved.</p>
           </Col>
           <Col>
-            <h4 className="mb-2">Quick Links</h4>
+            <h6 className="mb-1">Quick Links</h6>
             <div className="mb-1">Home</div>
-            <div className="mb-1">Report Lost Item</div>
+            <div className="mb-1">Lost Items</div>
             <div className="mb-1">Claim Found Item</div>
           </Col>
           <Col>
-            <h4 className="mb-2">Contact</h4>
+            <h6 className="mb-1">Contact</h6>
             <p className="mb-1">Department of Information and Computer Sciences</p>
             <p className="mb-1">University of Hawaii</p>
             <p className="mb-1">Honolulu, HI 96822</p>
             <p className="mb-1">Contact: lostandfound@hawaii.edu</p>
           </Col>
           <Col>
-            <h4 className="mb-2">Follow us on Social Media</h4>
-            <div className="d-flex justify-content-center"> {/* Center content horizontally */}
-              <Button variant="link" className="social-icon me-3" onClick={() => openSocialLink('https://www.instagram.com/uhm_ics/')}>
-                Instagram
+            <div>
+              <h6 className="mb-1">Follow Us on Social Media</h6>
+            </div>
+            <div>
+              <Button variant="link" className="social-icon me-2" onClick={() => openSocialLink('https://www.instagram.com/uhm_ics/')} style={{ fontSize: '24px' }}>
+                <i className="bi bi-instagram" />
               </Button>
-              <Button variant="link" className="social-icon" onClick={() => openSocialLink('https://twitter.com/uhmics?lang=en')}>
-                Twitter
+              <Button variant="link" className="social-icon" onClick={() => openSocialLink('https://twitter.com/uhmics?lang=en')} style={{ fontSize: '24px' }}>
+                <i className="bi bi-twitter" />
               </Button>
             </div>
           </Col>
