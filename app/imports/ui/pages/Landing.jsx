17f99b1f2c51f429c30c1c0a5ee236b9bbb11ab0
Michelle Rasavong <94037287+MRasavong@users.noreply.github.com>
2024-04-08 11:30:09 -1000

    Moved footer to bottom

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Container, Image, Row, Col } from 'react-bootstrap';
 
-/* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <>
+    {/* Background image */}
     <Image src="https://manoa.hawaii.edu/library/wp-content/uploads/2017/10/Sunny-Alcove.jpg" style={{ width: '100vw', filter: 'brightness(0.6)', position: 'absolute', zIndex: -1 }} />
-    <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col>
-          <h1 className="text-white mt-5" style={{ fontSize: '160px', fontWeight: 600 }}>Lost and Found</h1>
-          <a id="home-button" href="/list">Locate Lost Items</a>
-        </Col>
-      </Row>
-    </Container>
+    {/* Content container */}
+    <div style={{ position: 'relative', minHeight: 'calc(100vh - 56px)' }}>
+      <Container fluid className="py-3">
+        <Row className="align-items-center justify-content-center">
+          <Col className="text-center">
+            <h1 className="text-white mt-5" style={{ fontSize: '160px', fontWeight: 600 }}>Lost and Found</h1>
+            <a id="home-button" href="/list">Locate Lost Items</a>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   </>
 );
 
