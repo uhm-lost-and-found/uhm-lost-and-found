5945464b499fbd6fc8329b1ad0253373194e5b0d
Jalen Lum <808jalen@gmail.com>
2024-04-11 18:54:50 -1000

    Fixed landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -14,11 +14,11 @@ const Landing = () => (
       </Row>
       <Row className="mt-5 text-white">
         <Col>
-          <Image style={{ borderRadius: 10 }} fluid src="/images/item-page.png" />
+          <Image style={{ borderRadius: 10, height: 225 }} fluid src="/images/item-page.png" />
         </Col>
         <Col>
-          <h2>Reclaim your lost items.</h2>
-          <p>Welcome to the Lost and Found web application! This platform serves as a centralized catalog/database for lost items within the university community. Whether you're a student or faculty member, this user-friendly tool is designed to streamline the process of finding lost belongings.</p>
+          <h3>Reclaim your lost items.</h3>
+          <p>Welcome to the Lost and Found web application! This platform serves as a centralized catalog/database for lost items within the university community.</p>
           <p>Browse Lost Items: You can easily browse through the catalog of lost items to locate your missing belongings.</p>
           <p>View Item Details: Detailed information about each lost item, including where it was found and where it is located, is readily available for you.</p>
         </Col>
