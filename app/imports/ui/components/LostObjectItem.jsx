46f4bc5b85897025f1954010ba33b354e0913873
Jalen Lum <808jalen@gmail.com>
2024-04-18 13:00:40 -1000

    displaying

--- app/imports/ui/components/LostObjectItem.jsx
+++ app/imports/ui/components/LostObjectItem.jsx
@@ -1,30 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
 
 /** Renders a single row in the List Stuff table. See pages/ListItems.jsx. */
 const LostObjectItem = ({ lostObject }) => {
-  <Card className="h-100">
-    <Card.Img className="object-image" src={lostObject.image} alt={lostObject.name} />
-    <Card.Body>
-      <Card.Title>{lostObject.name}</Card.Title>
-      <Card.Subtitle>{lostObject.currentDepartment}</Card.Subtitle>
-      <Card.Text>
-        Date Found: {lostObject.dateFound}
-        <br />
-        Location Found: {lostObject.locationFound}
-      </Card.Text>
-      <button
-        type="button"
-        className="btn text-white"
-        data-bs-toggle="modal"
-        data-bs-target={lostObject._id}
-        style={{ backgroundColor: '#00502f', borderRadius: 60 }}
-      >
-        Read More
-      </button>
-    </Card.Body>
-  </Card>;
+  const { currentUser } = useTracker(() => ({
+    currentUser: Meteor.user() ? Meteor.user().username : '',
+  }), []);
+
+  return (
+    <Card className="h-100">
+      <Card.Img className="object-image" src={lostObject.image} alt={lostObject.name} />
+      <Card.Body>
+        <Card.Title>{lostObject.name}</Card.Title>
+        <Card.Subtitle>{lostObject.currentDepartment}</Card.Subtitle>
+        <Card.Text>
+          Date Found: {lostObject.dateFound}
+          <br />
+          Location Found: {lostObject.locationFound}
+        </Card.Text>
+        <button
+          type="button"
+          className="btn text-white"
+          data-bs-toggle="modal"
+          data-bs-target={lostObject._id}
+          style={{ backgroundColor: '#00502f', borderRadius: 60 }}
+        >
+          Read More
+        </button>
+      </Card.Body>
+    </Card>
+  );
+  
+  
+  
+  
+  
+  
+  
 };
 
 // Require a document to be passed to this component.
