182e0615385873271366135948e7cd55ec760635
Victor Pagan <vicpagan808@gmail.com>
2024-05-07 16:33:19 -1000

    Update DepartmentItem.jsx
    Fully implemented UI side of account deletion with confirmation modal popups.
    
    Backend will use the removeItem function to delete account from the database.

--- app/imports/ui/components/DepartmentItem.jsx
+++ app/imports/ui/components/DepartmentItem.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardHeader, CardBody, Button } from 'react-bootstrap';
+import { Card, CardHeader, CardBody, Button, Modal } from 'react-bootstrap';
 
 /** Renders a single row in the List Stuff table. See pages/ListItems.jsx. */
 const DepartmentItem = ({ department }) => {
 
+  const [showModal, setShowModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+  const handleDeleteClose = () => setShowDeleteModal(false);
+  const handleDeleteShow = () => setShowDeleteModal(true);
+
   const removeItem = (docID) => {
+    handleClose();
+    handleDeleteShow();
     console.log(`The item to remove is ${docID}`);
   };
 
@@ -20,11 +30,57 @@ const DepartmentItem = ({ department }) => {
       </CardBody>
       <Button
         variant="danger"
-        onClick={() => removeItem(department._id)}
+        onClick={handleShow}
         style={{ borderRadius: 60, border: 'none' }}
       >
         Delete Department Account
       </Button>
+      <Modal
+        show={showModal}
+        onHide={handleClose}
+        centered
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header style={{ borderColor: '#282828', backgroundColor: '#DC3545' }}>
+          <Modal.Title>
+            <h1 style={{ color: 'white' }}>STOP!!!</h1>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h5>Deleting Department Account: {department.username}</h5>
+          <p><strong>Are you sure? There is no going back.</strong></p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" style={{ borderRadius: 60, border: 'none' }} onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="danger" style={{ borderRadius: 60, border: 'none' }} onClick={() => removeItem(department._id)}>
+            CONFIRM DELETE
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal
+        show={showDeleteModal}
+        onHide={handleDeleteClose}
+        centered
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header style={{ borderColor: '#282828', backgroundColor: '#198754' }}>
+          <Modal.Title>
+            <h1 style={{ color: 'white' }}>Success</h1>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h5>Successfully Deleted Department Account: {department.username}</h5>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" style={{ borderRadius: 60, border: 'none' }} onClick={handleDeleteClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Card>
   );
 };
