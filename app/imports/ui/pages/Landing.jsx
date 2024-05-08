fd648daa5eabf4a61981a7dc8cb15887153414aa
Jalen Lum <808jalen@gmail.com>
2024-05-08 01:53:19 -1000

    added aos

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Image, Row, Col } from 'react-bootstrap';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
+AOS.init();
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
+
   <>
     <Image src="https://manoa.hawaii.edu/library/wp-content/uploads/2017/10/Sunny-Alcove.jpg" style={{ width: '100vw', height: '100vh', filter: 'brightness(0.6)', position: 'absolute', zIndex: -1, objectFit: 'cover' }} />
     <Container id="landing-page" className="py-3">
-      <Row className="align-middle text-center">
-        <Col>
-          <h1 className="text-white" style={{ fontSize: '160px', fontWeight: 600 }}>Lost and Found</h1>
-          <a id="home-button" href="/list">Locate Lost Items</a>
-        </Col>
-      </Row>
+      <div data-aos="fade-up" data-aos-duration="2000">
+        <Row className="align-middle text-center">
+          <Col>
+            <h1 className="text-white" style={{ fontSize: '160px', fontWeight: 600 }}>Lost and Found</h1>
+            <a id="home-button" href="/list">Locate Lost Items</a>
+          </Col>
+        </Row>
+      </div>
       <Row className="mt-5 text-white">
         <Col>
           <Image style={{ borderRadius: 10, height: 225, display: 'block', margin: 'auto' }} className="justify-content-center" fluid src="/images/airpods.png" />
